30ee79122fd83e91a93a6f6d71bb432fff48aa7f
keyuchang <aceking,ke@gmail.com>
2022-08-13 12:06:48 +0800

    *: add hightlight.js

--- deploy.sh
+++ deploy.sh
@@ -18,5 +18,4 @@ git push -f git@github.com:acekingke/acekingke.github.io.git master
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-cd -
 
